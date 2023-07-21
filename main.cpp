9a8921b12ec2562bc4d00892652b4c0c0d709251
TazoArT-27 <shafayat.tazoar27@gmail.com>
2023-07-21 21:45:06 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int addNumbers(int num1, int num2){
+    int sum = num1+num2;
+    return sum;
+}
+
+int main(){
+    cout << "Shafayat" << endl;
+    int sum = addNumbers(1, 2);
+    cout<<"The sum is: "<<sum<<endl;
+    return 0;
+}
